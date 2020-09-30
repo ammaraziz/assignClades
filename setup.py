82a18a1b01df028cbff1bf0e5891779f48d2fdba
Ammar Aziz <spidermine63@gmail.com>
2020-09-30 13:22:46 +1000

    corrected project structure

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(name='CladeAssigner',
       version='0.1',
-      description='Return clades for given input sequences',
+      description='Return HA clades for given input sequences',
       author='Ammar Aziz',
-      author_email='jrj.healey@gmail.com',
+      author_email='ammar.aziz@mh.org.au',
       license='MIT',
-      packages=['prymer'],
-      install_requires=['biopython', 'seqanpy', 'pandas'],
+      packages=['assignClades.py'],
+      install_requires=['biopython', 'pandas'],
       zip_safe=False)
